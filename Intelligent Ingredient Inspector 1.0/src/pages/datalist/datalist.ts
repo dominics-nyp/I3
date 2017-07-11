1f645a9f5d0b19d100948b5072315593420ebca2
underhandace <underhandface@gmail.com>
2017-07-11 16:49:27 +0800

    ikjiok

--- Intelligent Ingredient Inspector 1.0/src/pages/datalist/datalist.ts
+++ Intelligent Ingredient Inspector 1.0/src/pages/datalist/datalist.ts
@@ -33,15 +33,15 @@ allergy:any;
  
  public user: FirebaseListObservable<any>;
  public Allergies:FirebaseListObservable<any>;
- public profile:FirebaseListObservable<any>;
+
  
 
 public Allergies1:Array<{allergies: string, checked: boolean}>;//array contains a list of objects. Each object needs to have a allergy:xx and checked: true/false
 public user1;
-public profile1;
+public profile1:Array<{allergies: string, checked: boolean}>=[];
 
 
-  User: Array<{Name: string, Allergies: string,  icon: string,showUser: boolean}> = [];
+ User: Array<{Name: string, Allergies: string,  icon: string,showUser: boolean}> = [];
  
 
 
@@ -50,61 +50,102 @@ public profile1;
     public af: AngularFire,
     public navParams: NavParams) {
 
-    this.user = af.database.list('/user/' + firebase.auth().currentUser.uid + '/profile/' +'/allergies/'+ '/0/');
+    this.user = af.database.list('/user/' + firebase.auth().currentUser.uid + '/profile/' + '/allergies');
     this.Allergies = af.database.list('/ingredientDB/');
-    this.Allergies1 = [];
+    
+    
+
+    // console.log("navParams: " + navParams.get("profile"));
     this.user1= navParams.get("user");
     this.profile1= navParams.get("profile");
 
 
     af.database.list('/ingredientDB/', { preserveSnapshot: true})
     .subscribe(snapshots=>{
+
+      //profile1 = user's allergies
       
         snapshots.forEach(snapshot => {
         
+     // this.Allergies1 = [];
+
          console.log(snapshot.key, snapshot.val());
-         // console.log("allergies :"+ this.profile1);
-          console.log("user :"+ this.user1);
-           
-         //push allergies into Allergies1
-         if(this.profile1 ==snapshot.val().allergies)
-         {
-           snapshot.val().checked == true
+    //  console.log("allergies :"+ this.profile1);
+         
+       
+    //      //push allergies into Allergies1
+    //      if(this.profile1 == snapshot.val().allergies)
+    //   {
+    //        snapshot.val().checked == true;
           
-           console.log("checked is true");
+    //      //  console.log("checked is true");
+
+    //        //push allergies to allergies; turn toggle to true
+    //       this.Allergies1.push({
+    //       allergies: snapshot.val().allergies,
+    //        checked: true
            
-         this.Allergies1.push({
-          allergies: snapshot.val().allergies,
+    //       });
+
+        
+    //  }
+
+    //      else
+    //  {
+    //    //    console.log("checked is false");
+    //         this.Allergies1.push({
+    //       allergies: snapshot.val().allergies,
+    //        checked: false
+    //       });
+    //  }
+
+
+console.log("this.profile : " + JSON.stringify(this.profile1));
+    this.profile1.forEach(element => {
+ this.Allergies1 = [];
+     let flag:boolean =false;
+         
+
+        if(element == snapshot.val().allergies)
+        {
+            
+           snapshot.val().checked == true;
+
+           this.Allergies1.push({
+           allergies: snapshot.val().allergies,
            checked: true
            
           });
-           
-          console.log("allergies1:" + this.Allergies1)
-         
-         
-          
-         }
 
-         else
-         {
-           console.log("checked is false");
-            this.Allergies1.push({
-          allergies: snapshot.val().allergies,
-           checked: false
+//set the flag to true
+      flag=true;
+      console.log("flag: " + JSON.stringify(flag));
+
+        }
+
+ //check the flag and push the Allergies1 if the flag is false 
+        else {
+              // flag =false;
+           snapshot.val().checked == false;
+          //   console.log("hello");
+          this.Allergies1.push({
+            allergies: snapshot.val().allergies,
+            checked: false
           });
+         console.log("flag: " + JSON.stringify(flag));
+        }
 
-          
+
+    });
+
         
-           
-          
-         }
-          
-      }
 
+    
+        }
       
 
       
-      
+    
      
 
     );
@@ -112,7 +153,11 @@ public profile1;
     
     
     });
-  }
+
+   
+    }
+  
+
 
   addchecked(allergy):void{
   
@@ -129,25 +174,19 @@ public profile1;
 
 
 
-   //   this.initializeItems();
+      initializeItems();
 
+        initializeItems(): void{
 
-
-initializeItems(): void{
-  this.allergy =[
+          this.allergy = [this.af.database.list('/ingredientDB/')];
   
-   /*'peanut',
-   'chicken',
-   'eggs',
-   'milk',
-   'cheese',
-   'strawberries',
-   'nuts',
-   'a',
-   'duck',*/  
-  ];
+  
+    
+  
  
 }
+
+
   
 
 getItems(ev: any) {
@@ -160,7 +199,7 @@ getItems(ev: any) {
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.allergy = this.allergy.filter((allergies) => {
-       return (allergies.toLowerCase().indexOf(val.toLowerCase()) > -1);
+       return (allergies.toString().toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
    }
   }
